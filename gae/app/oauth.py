232ee0791b94ddf7d81df856f4a8cd28ce70856b
Jim Renwick <jimrenwick@gmail.com>
2014-01-22 05:13:29 +0000

    Add some comments and a distro-clean rule.

--- gae/app/oauth.py
+++ gae/app/oauth.py
@@ -1,4 +1,5 @@
-
+# Description:
+#   Utilities for authorizing http requests.
 
 from google.appengine.api import users
 from google.appengine.ext import ndb
@@ -9,10 +10,21 @@ from oauth2client.appengine import StorageByKeyName
 
 
 def GetEndpointsUser():
+  """Since the endpoints.User doesn't have user_id set, we just use User.
+
+  Returns:
+    user: (User) Gets the user struct.
+  """
   user = users.get_current_user()
+  return user
 
 
 def EndpointsGetAuthorizedHttp():
+  """Sign an Http with user credentials.
+
+  Returns:
+    Http: An authorized Http connection.
+  """
   user = users.get_current_user()
   storage = StorageByKeyName(
     CredentialsModel, user.user_id(), 'credentials')
